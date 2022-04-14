d657b6d1e9ca857c23860fabc76c69687f4ffb78
Isaac Prado <icpereiradoprado@gmail.com>
2022-04-14 15:53:46 -0300

    Creation of the Position class

---
+++ ConsoleChess/Board/Position.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleChess.Board
+{
+    class Position
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Position(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return $"{Linha}, {Coluna}";
+        }
+    }
+}
--- ConsoleChess/Program.cs
+++ ConsoleChess/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using ConsoleChess.Board;
+
+namespace ConsoleChess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position position = new Position(3,4);
+            Console.WriteLine(position);
+        }
+    }
+}
