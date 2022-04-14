4bd8756dd80779351024edb445ddf3a1c8865a0b
Isaac Prado <icpereiradoprado@gmail.com>
2022-04-14 16:13:07 -0300

    Starting the implementation of the board

--- ConsoleChess/Board/Board.cs
+++ ConsoleChess/Board/Board.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace ConsoleChess.Board
 {
-    internal class Board
+    class Board
     {
+        public int Lines { get; set; }
+        public int Columns { get; set; }
+        private Piece[,] Pieces;
+
+        public Board(int lines, int columns)
+        {
+            Lines = lines;
+            Columns = columns;
+            Pieces = new Piece[Lines,Columns];
+        }
     }
 }
